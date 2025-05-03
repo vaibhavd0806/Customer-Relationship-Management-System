3a9a70671b692a62eb329ef71d724db262539c32
Vaibhav Dhotre <88840347+vaibhavd0806@users.noreply.github.com>
2025-05-03 11:48:25 +0530

    Update db.py

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ import psycopg2
 
 def get_connection():
     return psycopg2.connect(
-        host="localhost",
+        host="**enter_your_hostname**",
         database="crm_system",
         user="postgres",
         password="admin123"
